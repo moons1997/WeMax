553457ef88f7cf56079935b25492fdf11b41fa38
moons1997 <50578926+moons1997@users.noreply.github.com>
2021-06-30 16:31:37 +0500

    last

--- app/js/index.js
+++ app/js/index.js
@@ -7,6 +7,27 @@ const { imgLoadHelper } = new FuncsHelper();
 
 // Invoking Functions from Helper
 
+function problemSection() {
+  let tl = gsap.timeline();
+
+  tl.fromTo('.problem-dots1', 0.5, { opacity: 0 }, { opacity: 1 })
+    .fromTo('.problem-text .bg', 0.5, { width: 0 }, { width: '100%' })
+    .fromTo('.problem-text .text', 0.5, { opacity: 0 }, { opacity: 1 })
+    .fromTo('.problem-dots2', 0.5, { opacity: 0 }, { opacity: 1 }, '-=0.5')
+    .fromTo('.problem-plus', 0.5, { opacity: 0 }, { opacity: 1 })
+    .fromTo('.problem-plus', 0.5, { transform: 'rotate(-90deg)' }, { transform: 'rotate(0deg)' })
+    .fromTo('.problem-line .first', 0.5, { x: '-100%' }, { x: '0' })
+    .fromTo('.problem-line .second', 0.5, { x: '-100%' }, { x: '0' }, '-=0.25')
+    .fromTo('.problem-category__item .bg', 0.5, { width: 0 }, { width: '100%' })
+    .fromTo('.problem-category__later', 0.5, { opacity: 0 }, { opacity: 1 })
+    .fromTo('.problem-category__text', 0.5, { opacity: 0 }, { opacity: 1 }, '-=0.5')
+    .fromTo('.problem-text .bg', 0.5, { width: '100%' }, { width: '0' })
+    .to('.problem-category__item .bg', 0.5, { width: '0' }, '-=0.5');
+}
+let tl = gsap.timeline();
+tl.fromTo('.line-top', 3, { opacity: 0 }, { opacity: 1 });
+tl.fromTo('.line-side', 3, { y: '-100%' }, { y: 0 });
+
 function fullPage() {
   new fullpage('#fullpage', {
     licenseKey: 'YOUR KEY HERE',
@@ -25,5 +46,6 @@ function fullPage() {
 
 function init() {
   fullPage();
+  problemSection();
 }
 init();
